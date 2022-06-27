3cf29abc21c14e14ca43b0e6c345f9b98efa5ae7
rmas2 <89786801+rmas2@users.noreply.github.com>
2022-06-27 13:38:30 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,10 +1,29 @@
 const { Sequelize } = require('sequelize');
 
 
-const dbSequelize = new Sequelize('sosmulher', 'root', 'aluno', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
+//const dbSequelize = new Sequelize('sosmulher', 'root', 'aluno', {
+//    host: 'localhost',
+//    dialect: 'mysql'
+//});
+
+sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 
 module.exports = dbSequelize;
